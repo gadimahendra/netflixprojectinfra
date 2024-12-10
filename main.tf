d603f698f992968bbe28878639fd3fb9db64feb4
gadimahendra <91515094+gadimahendra@users.noreply.github.com>
2024-12-10 18:32:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0166fe664262f664c"
 instance_type = "t2.medium"
 key_name = "rahull"
-vpc_security_groups_ids = ["sg-05f044979e305302e"]
+vpc_security_groups_ids = ["sg-02cdaee359c741614"]
 tags = {
 Name = var.instance_names[count.index]
 }
